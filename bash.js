4cc1c532e654cf098524ac7d21be4d985db72f52
sheriffahmed <sheriff.v.ahmed@gmail.com>
2022-04-22 15:31:20 -0400

    ls module exported to pwd module

--- bash.js
+++ bash.js
@@ -1,3 +1,5 @@
+// imported from pwd.js - prints working directory if prompt = "pwd"
 const pwd = require('./pwd');
 
 pwd();
+
--- pwd.js
+++ pwd.js
@@ -1,9 +1,11 @@
+const ls = require('./ls')
 module.exports = function () {
   const { cwd } = require("process");
   process.stdout.write("prompt > ");
 process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
   if (cmd === "pwd") process.stdout.write(cwd());
+  else if(cmd === 'ls') ls();
   else process.stdout.write("You typed: " + cmd);
   process.stdout.write("\nprompt > ");
 });
