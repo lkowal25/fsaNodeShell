856f9c0caeba33cc16583677aa3a6c5f7de88594
leland <ec2-user@ip-172-31-88-144.ec2.internal>
2022-04-22 21:37:35 +0000

    installed template literal in cat.js and required .js in cmd prmpt

--- bash.js
+++ bash.js
@@ -7,7 +7,7 @@ const cat = require('./cat');
 process.stdout.write("prompt > ");
 process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
-  let fileName = cmd.split(' ').slice(1);
+  const fileName = cmd.split(' ').slice(1);
   if (cmd === 'pwd') pwd();
   else if(cmd === 'ls') ls();
   else if(cmd.split(' ')[0] === 'cat') cat(...fileName);
--- cat.js
+++ cat.js
@@ -1,9 +1,8 @@
 module.exports = function(args) {
   const { readFile } = require('fs');
   
-  
 
-readFile('./'+ args + '.js', 'utf8', (err, data) => {
+readFile(`./${args}`, 'utf8', (err, data) => {
   if (err) throw err;
    process.stdout.write(data)
    process.stdout.write('prompt > ');
