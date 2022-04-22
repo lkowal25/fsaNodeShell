99f1c9f530cca2d469b42c64dbb77acbc72e636f
leland <ec2-user@ip-172-31-88-144.ec2.internal>
2022-04-22 20:00:28 +0000

    moved functionality from pwd to bash

--- bash.js
+++ bash.js
@@ -1,5 +1,15 @@
 // imported from pwd.js - prints working directory if prompt = "pwd"
 const pwd = require('./pwd');
+const ls = require('./ls')
+// const cat = require('./cat');
 
-pwd();
 
+process.stdout.write("prompt > ");
+process.stdin.on("data", (data) => {
+  const cmd = data.toString().trim();
+  if (cmd === 'pwd') pwd();
+  else if(cmd === 'ls') ls();
+  // else if(cmd === 'cat') cat(...args);
+  else process.stdout.write("You typed: " + cmd);
+  process.stdout.write("\nprompt > ");
+});
