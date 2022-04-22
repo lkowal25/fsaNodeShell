015dbb26ad5f52158fdb2564d6f73d97377de04c
sheriffahmed <sheriff.v.ahmed@gmail.com>
2022-04-22 14:52:48 -0400

    changed to common JS

--- bash.js
+++ bash.js
@@ -1,11 +1,10 @@
-// process.cwd() === import { cwd } from 'process';
+// process.cwd() ===
+const { cwd } = require("process");
 process.stdout.write("prompt > ");
 
 process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
-
-  if (cmd === "pwd") process.stdout.write(process.cwd());
+  if (cmd === "pwd") process.stdout.write(cwd());
   else process.stdout.write("You typed: " + cmd);
-
   process.stdout.write("\nprompt > ");
 });
