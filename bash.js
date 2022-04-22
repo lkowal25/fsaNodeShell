1de6f4f03dcf3f24b612753d8ca432f08b41ce8d
sheriffahmed <sheriff.v.ahmed@gmail.com>
2022-04-22 15:02:26 -0400

    first export modules for pwd

--- bash.js
+++ bash.js
@@ -1,10 +1,3 @@
-// process.cwd() ===
-const { cwd } = require("process");
-process.stdout.write("prompt > ");
+const pwd = require('./pwd');
 
-process.stdin.on("data", (data) => {
-  const cmd = data.toString().trim();
-  if (cmd === "pwd") process.stdout.write(cwd());
-  else process.stdout.write("You typed: " + cmd);
-  process.stdout.write("\nprompt > ");
-});
+pwd();
