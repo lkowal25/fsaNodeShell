dc1405b8297d44928fa68ee4fb9d5b4f24ef7d70
sheriffahmed <sheriff.v.ahmed@gmail.com>
2022-04-22 13:17:11 -0400

    pwd prints the working directory

--- bash.js
+++ bash.js
@@ -1,8 +1,11 @@
+// process.cwd() === import { cwd } from 'process';
 process.stdout.write("prompt > ");
-process.stdin.on('data', (data) => {
+
+process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
-  process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt > ');
 
-})
+  if (cmd === "pwd") process.stdout.write(process.cwd());
+  else process.stdout.write("You typed: " + cmd);
 
+  process.stdout.write("\nprompt > ");
+});
